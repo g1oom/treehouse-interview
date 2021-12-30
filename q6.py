f7e91cc44ad77fc9e436617a73b7e066e9f89bed
g1oom <albertowj2001@gmail.com>
2021-12-30 14:56:58 +0800

    added question 6

---
+++ q6.py
@@ -0,0 +1,34 @@
+import datetime
+
+# all possible date formats in text file
+formats = ('%Y/%m/%d', '%m/%d/%Y', '%d/%m/%Y', '%d %b %Y')
+
+dateCount = 0
+content = ""
+
+with open("q6/example.txt", "r") as f:
+    content = f.read()
+
+content = content.split()
+
+# testing formats a, b, and c
+for str in content:
+    for format in formats[:3]:
+        try:
+            newStr = datetime.datetime.strptime(str, format)
+            dateCount += 1
+        except:
+            pass
+
+# testing format d
+for i in range(len(content) - 2):
+    # check 3 words at once
+    str = " ".join([content[i], content[i + 1], content[i + 2]])
+    
+    try:
+        newStr = datetime.datetime.strptime(str, formats[-1])
+        dateCount += 1
+    except:
+        pass
+
+print(dateCount)
